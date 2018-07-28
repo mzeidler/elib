5da1b988fdb516e1fcc25ba36f5b88c0a8414144
mzeidler <mzeidler3@gmail.com>
2018-07-28 14:56:40 +0200

    Grid Filter

--- src/main/java/de/markozeidler/elib/window/MainWindow.java
+++ src/main/java/de/markozeidler/elib/window/MainWindow.java
@@ -7,6 +7,7 @@ import java.util.Set;
 import javax.inject.Inject;
 
 import com.vaadin.icons.VaadinIcons;
+import com.vaadin.server.SerializablePredicate;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Grid;
@@ -46,39 +47,43 @@ public class MainWindow extends Window implements Serializable {
 	private TextArea textArea;
 
 	private UIBuilder uiBuilder;
-	
+
 	/**
 	 * Windows
 	 */
 	private ThemesWindow themesWindow;
-	
+
 	private RemoveDocumentWindow removeDocumentWindow;
-		
+
 	private DocumentWindow documentWindow;
+
+	private TextField titleFilter;
+	
+	private NativeSelect<Theme> themeFilter;
 	
 	/**
 	 * Buttons
 	 */
-	
+
 	private Button bAdd;
-	
+
 	private Button bRemove;
-	
+
 	private Button bEditDocument;
-	
+
 	private Button bEditDocumentContent;
-	
+
 	private Button bSave;
-	
+
 	private Button bAddCode;
-	
+
 	private Button bSelectionMode;
-	
+
 	private Button bThemes;
-	
+
 	@Inject
-	public MainWindow(HeaderLayout headerLayout, UIBuilder uiBuilder, ThemesWindow themesWindow, DataRepository dataRepository,
-			RemoveDocumentWindow removeDocumentWindow, DocumentWindow documentWindow) {
+	public MainWindow(HeaderLayout headerLayout, UIBuilder uiBuilder, ThemesWindow themesWindow,
+			DataRepository dataRepository, RemoveDocumentWindow removeDocumentWindow, DocumentWindow documentWindow) {
 		this.headerLayout = headerLayout;
 		this.uiBuilder = uiBuilder;
 		this.themesWindow = themesWindow;
@@ -111,93 +116,102 @@ public class MainWindow extends Window implements Serializable {
 	}
 
 	private void initGrid() {
-		
+
 		// Initialize Grid
 		grid = new Grid<Document>();
 		grid.setSizeFull();
 		grid.setSelectionMode(SelectionMode.SINGLE);
-		
+
 		// Columns
 		grid.addColumn(Document::getTheme).setCaption("Theme").setId("theme");
 		grid.addColumn(Document::getTitle).setCaption("Title").setId("title");
-		
-		grid.addColumn(Document::getCreated, new DateRenderer("%1$td.%1$tm.%1$tY", Locale.ENGLISH)).setCaption("Created");
-		grid.addColumn(Document::getUpdated, new DateRenderer("%1$td.%1$tm.%1$tY", Locale.ENGLISH)).setCaption("Updated");			
-		
+
+		grid.addColumn(Document::getCreated, new DateRenderer("%1$td.%1$tm.%1$tY", Locale.ENGLISH))
+				.setCaption("Created");
+		grid.addColumn(Document::getUpdated, new DateRenderer("%1$td.%1$tm.%1$tY", Locale.ENGLISH))
+				.setCaption("Updated");
+
 		// Filter
-	    TextField titleFilter = new TextField();
-	    titleFilter.setWidth("100%");
-	    titleFilter.addStyleName(ValoTheme.TEXTFIELD_TINY);
-	    titleFilter.setPlaceholder("Filter");       
-	    titleFilter.addValueChangeListener(event -> {        	
-        	dataRepository.getDocumentDataProvider().setFilter(Document::getTitle, documentTitle -> {
-                if (documentTitle == null) {
-                    return false;
-                }
-                return documentTitle.toLowerCase().contains(event.getValue().toLowerCase());
-            });
-        });
-        
-        
-        NativeSelect<Theme> themeFilter = new NativeSelect<>(null, dataRepository.findAllThemes());
-        themeFilter.setWidth("100%");       
-        themeFilter.addValueChangeListener(event -> {
-        	dataRepository.getDocumentDataProvider().setFilter(Document::getTheme, theme -> {
-        		if (event.getValue() == null) {
-        			return true;
-        		}
-                if (theme == null) {
-                    return false;
-                }
-                return theme.getId().equals(event.getValue().getId());
-            });
-        	
-        });
-        
-        
-        HeaderRow filterRow = grid.appendHeaderRow();
-        filterRow.getCell("title").setComponent(titleFilter);
-        filterRow.getCell("theme").setComponent(themeFilter);
-        
+		titleFilter = new TextField();
+		titleFilter.setWidth("100%");
+		titleFilter.addStyleName(ValoTheme.TEXTFIELD_TINY);
+		titleFilter.setPlaceholder("Filter");
+		titleFilter.addValueChangeListener(event -> {
+			dataRepository.getDocumentDataProvider().refreshAll();
+		});
+
+		themeFilter = new NativeSelect<>(null, dataRepository.findAllThemes());
+		themeFilter.setWidth("100%");
+		themeFilter.addValueChangeListener(event -> {
+			dataRepository.getDocumentDataProvider().refreshAll();
+		});
+
+		SerializablePredicate<Document> filter = new SerializablePredicate<Document>() {
+			@Override
+			public boolean test(Document document) {
+				
+				// Theme Filter
+				boolean matchesTheme = true;
+				if (themeFilter.getSelectedItem().isPresent()) {
+					Integer themeId = document.getTheme() == null ? null : document.getTheme().getId();
+					matchesTheme = themeFilter.getSelectedItem().get().getId().equals(themeId);
+				}
+
+				// Title Filter
+				boolean matchesTitle = true;
+				if (!"".equals(titleFilter.getValue())) {
+					matchesTitle = document.getTitle().toLowerCase().contains(titleFilter.getValue().toLowerCase());					
+				}
+				
+				return matchesTitle && matchesTheme;
+			}
+		};
+
+		dataRepository.getDocumentDataProvider().setFilter(filter);
+
+
+		HeaderRow filterRow = grid.appendHeaderRow();
+		filterRow.getCell("title").setComponent(titleFilter);
+		filterRow.getCell("theme").setComponent(themeFilter);
+
 		grid.setDataProvider(dataRepository.getDocumentDataProvider());
 		dataRepository.setGrid(grid);
 	}
-	
+
 	private void initGrid_old() {
 
-		//**********************************************************************************
+		// **********************************************************************************
 		// TODO: UNDER CONSTRUCTION
-		
+
 		grid.addSelectionListener(event -> {
 			Set<Document> selected = event.getAllSelectedItems();
 
-			
 			if (selected.size() > 0) {
 
 				Document doc = (Document) selected.toArray()[0];
 				textArea.setValue(doc.getTitle());
 
 				if (doc.getContent() != null) {
-					//textArea.setValue(doc.getContent());
+					// textArea.setValue(doc.getContent());
 					textArea.setValue(doc.getTitle());
 				}
 			}
 
 			Notification.show(selected.size() + " items selected");
 		});
-		//**********************************************************************************
+		// **********************************************************************************
 	}
-	
+
 	private void initButtons() {
 
 		bAdd = uiBuilder.button(VaadinIcons.FILE_ADD, "Add Document").build();
 		bAdd.addClickListener(new Button.ClickListener() {
 			public void buttonClick(ClickEvent event) {
-				documentWindow.init(null);				
+				documentWindow.init(null);
 				UI.getCurrent().addWindow(documentWindow);
 			}
 		});
-		
+
 		bRemove = uiBuilder.button(VaadinIcons.TRASH, "Remove Documents").build();
 		bRemove.addClickListener(new Button.ClickListener() {
 			public void buttonClick(ClickEvent event) {
@@ -208,27 +222,27 @@ public class MainWindow extends Window implements Serializable {
 				}
 			}
 		});
-		
+
 		bEditDocument = uiBuilder.button(VaadinIcons.EDIT, "Edit Document").build();
 		bEditDocument.addClickListener(new Button.ClickListener() {
 			public void buttonClick(ClickEvent event) {
 				Set<Document> selectedDocuments = grid.getSelectedItems();
 				if (selectedDocuments.size() == 1) {
 					selectedDocuments.forEach(action -> {
-						documentWindow.init(action);				
+						documentWindow.init(action);
 						UI.getCurrent().addWindow(documentWindow);
 					});
 				}
 			}
 		});
-		
+
 		bEditDocumentContent = uiBuilder.button(VaadinIcons.EDIT, "Edit Document Content").build();
 		bEditDocumentContent.addClickListener(new Button.ClickListener() {
 			public void buttonClick(ClickEvent event) {
-				// 
+				//
 			}
-		});		
-		
+		});
+
 		bSave = uiBuilder.button(VaadinIcons.DATABASE, "Save Document").build();
 		bSave.addClickListener(new Button.ClickListener() {
 			public void buttonClick(ClickEvent event) {
@@ -236,7 +250,7 @@ public class MainWindow extends Window implements Serializable {
 
 			}
 		});
-		
+
 		bAddCode = uiBuilder.button(VaadinIcons.FILE_CODE, "Add Code Snippet").build();
 		bAddCode.addClickListener(new Button.ClickListener() {
 			public void buttonClick(ClickEvent event) {
@@ -244,7 +258,7 @@ public class MainWindow extends Window implements Serializable {
 
 			}
 		});
-		
+
 		bSelectionMode = uiBuilder.button(VaadinIcons.TASKS, "Change Selection Mode").build();
 		bSelectionMode.addClickListener(new Button.ClickListener() {
 			public void buttonClick(ClickEvent event) {
@@ -255,7 +269,7 @@ public class MainWindow extends Window implements Serializable {
 				}
 			}
 		});
-				
+
 		bThemes = uiBuilder.button(VaadinIcons.FILE_TREE, "Manage Themes").build();
 		bThemes.addClickListener(new Button.ClickListener() {
 			public void buttonClick(ClickEvent event) {
@@ -264,13 +278,14 @@ public class MainWindow extends Window implements Serializable {
 			}
 		});
 	}
+
 	private VerticalLayout initLayouts() {
 
 		VerticalLayout upperToolbar = uiBuilder.VL().undefined().build();
 		upperToolbar.addComponents(bAdd, bEditDocument, bRemove, bSelectionMode, bThemes);
 
 		VerticalLayout lowerToolbar = uiBuilder.VL().undefined().build();
-		lowerToolbar.addComponents(bEditDocumentContent, bAddCode, bSave);		
+		lowerToolbar.addComponents(bEditDocumentContent, bAddCode, bSave);
 
 		HorizontalLayout mainUpperLayout = uiBuilder.HL().margin().full().build();
 		mainUpperLayout.addComponents(upperToolbar, grid);
@@ -286,11 +301,11 @@ public class MainWindow extends Window implements Serializable {
 		splitPanel.setFirstComponent(mainUpperLayout);
 		splitPanel.setSecondComponent(mainLowerLayout);
 		splitPanel.addStyleName(ValoTheme.SPLITPANEL_LARGE);
-		
+
 		VerticalLayout layout = uiBuilder.VL().noMargin().noSpacing().full().build();
 		layout.addComponents(headerLayout, splitPanel);
 		layout.setExpandRatio(splitPanel, 1);
-		
+
 		return layout;
 	}
 }
